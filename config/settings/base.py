1e12d946e3bf9bd6f7036c511d33919539da0607
hawking <ihawking@proton.me>
2024-08-13 15:52:17 +0800

    init

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,6 +132,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "common.middlewares.ConsoleMiddleware",
