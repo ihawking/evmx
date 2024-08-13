9ac2885b4fc9f3e705448f347b85a9cd08e3d93d
hawking <ihawking@proton.me>
2024-08-13 15:15:29 +0800

    init

--- config/settings/base.py
+++ config/settings/base.py
@@ -18,7 +18,7 @@ env = environ.Env()
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = False
+DEBUG = env.bool("DJANGO_DEBUG", False)
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
