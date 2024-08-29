157105bce8418d6516583e44305c763b3b55682c
hawking <ihawking@proton.me>
2024-08-29 11:43:09 +0800

    allowed hosts

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,7 +19,7 @@ env = environ.Env()
 DOMAIN = env.str("DOMAIN", None)
 USE_DOMAIN = DOMAIN is not None
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["*"]  # noqa: S104
 
 # GENERAL
 # ------------------------------------------------------------------------------
